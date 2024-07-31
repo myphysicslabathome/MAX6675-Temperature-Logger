6aa9078bf16ac03da80132653b5e0462d08d6886
Ujjwal Ghanta <myphysicslabathome@gmail.com>
2024-07-31 11:45:32 +0530

    Add files via upload

---
+++ MAX6675.py
@@ -0,0 +1,29 @@
+''' Program for temperture measurement by MAX6675 and T-type thermocouple(SPI Communication)
+    on 23/04/2024 
+'''
+
+# Establish Connection
+import eyes17.eyes
+p = eyes17.eyes.open()
+
+# Import python libreary 
+import time, math
+import numpy as np
+
+t0=time.time()                      # Time initialization
+while True:
+    p.SPI.start('CS1')
+    val = p.SPI.send16(0xFFFF)
+    t=time.time()-t0
+    p.SPI.stop('CS1')        
+    Temperature = (val>>3)*0.25
+    if(val&0x4):
+        print('Thermocouple not attached. : ')
+        #return
+    else:
+        print('Temp: ',Temperature)
+        file = open ("Max6675.dat", "a") # Appending file
+        file.write("{0:4.2f} {1:4.2f}\n".format(t,Temperature))
+    time.sleep(1)
+
+
